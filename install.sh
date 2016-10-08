24938b533322e685b5b456ebf87ade7bafbf94a7
Tony Xu <hhktony@gmail.com>
2016-10-08 17:05:10 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,7 @@ do
     ln -s $HOME/.vim/$i $HOME/.$i
 done
 
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
 vim -u $HOME/.vimrc +PlugInstall! +PlugClean! +qall
